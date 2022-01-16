e4a7b0dfef26548a276733cfc573d1c8d0918d1b
akshatha <nayakakshatha22@gmail.com>
2022-01-16 21:51:16 +0530

    initial commit

--- lib/MyProfile.dart
+++ lib/MyProfile.dart
@@ -15,20 +15,20 @@ class Resume extends StatelessWidget {
             child: Column(
                 mainAxisAlignment:MainAxisAlignment.spaceAround,
                 children:[
-                    Text("Shreya K H ", style: TextStyle(fontSize:24, fontWeight: FontWeight.bold),),
+                    Text("shreya K H ", style: TextStyle(fontSize:24, fontWeight: FontWeight.bold),),
                     Container(
                width: 200,
                 height: 200,
                 decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     image: DecorationImage(
-                      image: AssetImage('assets/images/photo.png'),
+                      image: AssetImage('assets/images/photo.jpeg'),
                       fit: BoxFit.fill,
                 ),
                 ),
               ),
                   
-              Text("Name : Shreya K H ", style: TextStyle(fontSize:18, fontWeight: FontWeight.bold),),
+              Text("Name : shreya K H ", style: TextStyle(fontSize:18, fontWeight: FontWeight.bold),),
               Text("Email : shreyakh79@gmail.com", style: TextStyle(fontSize:18, fontWeight: FontWeight.bold)),
               Text("USN : 4MT20AI046", style: TextStyle(fontSize:18, fontWeight: FontWeight.bold),),
               Text("Age : 19", style: TextStyle(fontSize:18, fontWeight: FontWeight.bold),),
